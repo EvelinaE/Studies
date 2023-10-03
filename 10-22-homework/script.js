817b32b3f4fef3359bcd960e8db8c70c43f11015
EvelinaE <144163627+EvelinaE@users.noreply.github.com>
2023-10-03 14:45:44 +0300

    Update script.js

--- 10-22-homework/script.js
+++ 10-22-homework/script.js
@@ -1,5 +1,5 @@
 
-for (let myNumber = 0; myNumber <=945; myNumber++) {
+for (let myNumber = 1; myNumber <=945; myNumber++) {
   if (myNumber % 9 === 0 && myNumber % 7 === 0 && myNumber % 5 === 0 && myNumber % 3 === 0) {
     console.log('FizzBuzzBiffFuzz')
   } else if (myNumber % 9 === 0 && myNumber % 5 === 0 && myNumber % 3 === 0) {
